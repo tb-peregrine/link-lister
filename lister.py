9d86643adbd1d0c8eb9ab3a7ef237be962d0891e
tb-peregrine <cameronarcher@tinybird.co>
2023-07-13 21:35:05 -0500

    Click commands, printing...

--- lister.py
+++ lister.py
@@ -1,25 +1,26 @@
-import sys
+import click
 import requests
 import csv
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse, urldefrag
 
 
-def process_pages(urls, subdirectory, rss):
+def process_pages(urls, subdirectory, rss, parent_class):
     # Create a dictionary to store the nested pages and their linked URLs
     nested_pages = {}
 
     for url in urls:
-        nested_pages[url] = process_page(url, subdirectory, rss)
+        nested_pages[url] = process_page(url, subdirectory, rss, parent_class)
 
     return nested_pages
 
 
-def process_page(url, subdirectory, rss):
+def process_page(url, subdirectory, rss, parent_class):
     # Create a set to store the visited pages
     visited_pages = set()
 
     try:
+        print(f'Processing {url}...', end=" ")
         # Send a GET request to the page
         response = requests.get(url)
         response.raise_for_status()
@@ -27,20 +28,26 @@ def process_page(url, subdirectory, rss):
         # Parse the HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
 
+        # Find the parent elements with the specified class
+        parent_elements = soup.find_all(class_=parent_class)
+
         # Find all the links on the page
         links = soup.find_all('a')
 
         # Extract the linked URLs within the subdirectory, excluding the RSS feed URL
         linked_urls = set()
-        for link in links:
-            href = link.get('href')
-            if href:
-                absolute_url = urljoin(url, href)
-                parsed_absolute_url = urlparse(absolute_url)
-                if parsed_absolute_url.path.startswith(subdirectory) and absolute_url != rss and absolute_url != url:
-                    cleaned_url = urldefrag(absolute_url)[0]  # Remove any anchors from the URL
-                    linked_urls.add(cleaned_url)
+        for parent_element in parent_elements:
+            links = parent_element.find_all('a')
+            for link in links:
+                href = link.get('href')
+                if href:
+                    absolute_url = urljoin(url, href)
+                    parsed_absolute_url = urlparse(absolute_url)
+                    if parsed_absolute_url.path.startswith(subdirectory) and absolute_url != rss and absolute_url != url:
+                        cleaned_url = urldefrag(absolute_url)[0]  # Remove any anchors from the URL
+                        linked_urls.add(cleaned_url)
 
+        print(f'Done! Found {len(linked_urls)} links.')
         return linked_urls
 
     except requests.exceptions.RequestException as e:
@@ -48,16 +55,17 @@ def process_page(url, subdirectory, rss):
         return set()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <RSS.xml URL> <subdirectory> <limit>")
-        sys.exit(1)
-
-    rss_url = sys.argv[1]
-    subdirectory = sys.argv[2]
-    limit = int(sys.argv[3])
-
+@click.command(help="Usage: python lister.py <RSS.xml URL> <subdirectory> [OPTIONS]")
+@click.argument('rss_url')
+@click.argument('subdirectory')
+@click.option('-l', '--limit', type=int, default='-1', help="Analyze the first <limit> URLs in the RSS feed")
+@click.option('-p', '--parent_class', default='blogpost-content', help="Search only for content nested underneath a parent element with <parent_class>")
+def main(rss_url, subdirectory, limit, parent_class):
     try:
+        if limit >= 0:
+            print(f'Processing first {limit} pieces of content in {rss_url}...')
+        else: print(f'Processing all content in {rss_url}...')
+
         # Send a GET request to the RSS feed
         response = requests.get(rss_url)
         response.raise_for_status()
@@ -68,7 +76,7 @@ if __name__ == '__main__':
         # Find all the URLs in the RSS feed
         urls = [item.link.text for item in soup.find_all('item')[:limit]]  # Limit the number of URLs using the slicing operator
 
-        nested_pages = process_pages(urls, subdirectory, rss_url)
+        nested_pages = process_pages(urls, subdirectory, rss_url, parent_class)
 
         # Create a list of all unique URLs
         all_urls = set(urls)
@@ -76,6 +84,7 @@ if __name__ == '__main__':
             all_urls.update(linked_urls)
 
         # Create the matrix
+        print("Building matrix...")
         matrix = []
         for row_url in all_urls:
             row = []
@@ -97,3 +106,6 @@ if __name__ == '__main__':
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while processing the RSS feed: {e}")
+
+if __name__ == '__main__':
+    main()
